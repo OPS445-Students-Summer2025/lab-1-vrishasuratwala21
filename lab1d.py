52026521923b20aeb4ac778ff9f2afccb9f97529
Vrisha Suratwala <vvsuratwala@myseneca.ca>
2025-05-11 15:35:19 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Description: This script performs basic math.'''
+
+#Define variables 
+x = 10
+y = 2
+z = 5
+
+#Perform basic calculation and print result 
+output = x + y * z
+print (str(x) + " + " + str(y) + " * " + str(z) + " = " + str(output))
