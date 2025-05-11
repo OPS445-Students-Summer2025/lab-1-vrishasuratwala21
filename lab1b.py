606a56278ee86583df6ea6a1a4ac0e5032b7fdba
Vrisha Suratwala <vvsuratwala@myseneca.ca>
2025-05-11 15:16:36 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: This script asks how old Isaac is.'''
+
+#Create a variable called "name"
+name = "Isaac"
+
+#Print message using the variable 
+print("How old are you " + name + "?")
