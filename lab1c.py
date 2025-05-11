b25d388252146683986130fad8c03e4b61cd1484
Vrisha Suratwala <vvsuratwala@myseneca.ca>
2025-05-11 15:25:47 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description: This script prints Isaac's age.'''
+
+#Assign name and age variables
+name = "Isaac"
+age = 72
+
+#Print the desired output
+print (name + ' is ' + str(age) + ' years old!')
